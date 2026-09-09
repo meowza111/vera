eb81e9efd1f3ff41203b5455663726cf2b2e3063
meowza111 <riannbaconguis01@gmail.com>
2026-09-09 15:43:33 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "/verakins", // required by Discord API, not shown in the client
-        state: "/verakins",     // this is what people actually see
+        state: "",     // this is what people actually see
         type: 3,               // Custom
       },
     ],
