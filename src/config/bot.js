ceda084722e5468868f7a06e1348ccf87a261dd9
meowza111 <riannbaconguis01@gmail.com>
2026-09-09 14:42:23 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "/verakins", // required by Discord API, not shown in the client
         state: "/verakins",     // this is what people actually see
         type: 3,               // Custom
       },
